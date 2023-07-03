781a171293e76a9389324264bcf60fc387561e71
Dhruv Sathavara <dhruvsathavara@Macbooks-MacBook-Pro.local>
2023-07-03 09:41:04 +0530

    metadat-profile

--- App/next.config.js
+++ App/next.config.js
@@ -1,7 +1,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-      apiKey: "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",
+      apiKey: "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",
       infuraProjectKey: "2DQRq820rLbznhFlkIbTkuYAyCS",
       infuraSecretKey: "33d97cf6366f9565421e36ff7e018e60",
       privateKey: "c6a24ce0e9c5caedef0f6ae7bbeebc36c389f72384cb0ffa36a35dde9b04c9a6",
